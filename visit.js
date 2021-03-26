63c3b225affbe2c77c545fc981f8f780588ae0aa
MP-C <mp.crcarvalho@gmail.com>
2021-03-26 11:28:28 +0100

    last version

---
+++ visit.js
@@ -0,0 +1,8 @@
+document.getElementById('test').style.color = 'white';
+document.getElementById('testt').style.color = 'white';
+document.getElementById('test1').style.color = 'white';
+document.getElementById('test2').style.color = 'white';
+document.getElementById('test3').style.color = 'white';
+document.getElementById('test4').style.color = 'white';
+document.getElementById('test5').style.color = 'white';
+document.getElementById('test6').style.color = 'white';
